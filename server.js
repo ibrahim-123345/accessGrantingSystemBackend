b87bb2132fe01981668683b47ad88556d7efcd20
IBRAHIM MOHAMED <ibrahimmohamed0784@gmail.com>
2025-09-30 15:46:27 +0300

    ..

--- server.js
+++ server.js
@@ -32,37 +32,37 @@ app.use(server.json())
 
 
 // Routes
-app.post('/api/departments', createDepartment)
-app.get('/api/departments',getAllDepartments)
-app.get('/api/departments/:id', getDepartmentById)
-app.put('/api/departments/:id', updateDepartment)
-app.delete('/api/departments/:id', deleteDepartment)
+app.post('/api/departments',authenticate, createDepartment)
+app.get('/api/departments',authenticate,getAllDepartments)
+app.get('/api/departments/:id',authenticate, getDepartmentById)
+app.put('/api/departments/:id',authenticate, updateDepartment)
+app.delete('/api/departments/:id',authenticate, deleteDepartment)
 
 
 
 //systems platform routes
-app.post('/api/systemsPlatforms',createSystemsPlatform)
-app.get('/api/systemsPlatforms', getAllSystemsPlatforms)
+app.post('/api/systemsPlatforms',authenticate,createSystemsPlatform)
+app.get('/api/systemsPlatforms', authenticate,getAllSystemsPlatforms)
 app.patch('/api/systemsPlatforms/:id',authenticate,updateSystemsPlatform)
 app.get('/api/systemsPlatforms/:id',authenticate,getSystemsPlatformById)
 app.delete('/api/systemsPlatforms/:id',authenticate,deleteSystemsPlatform)
-app.get('/api/systemFullDetails/:systemId',getSystemDetails)
+app.get('/api/systemFullDetails/:systemId',authenticate,getSystemDetails)
 
 
 
 //access types routes
-app.post('/api/accessTypes', createAccessType)
-app.get('/api/accessTypes', getAllAccessTypes)
-app.get('/api/accessTypes/:id', getAccessTypeById)
+app.post('/api/accessTypes',authenticate, createAccessType)
+app.get('/api/accessTypes',authenticate, getAllAccessTypes)
+app.get('/api/accessTypes/:id',authenticate, getAccessTypeById)
 app.put('/api/accessTypes/:id',authenticate, updateAccessType)
 app.delete('/api/accessTypes/:id',authenticate, deleteAccessType)
 
 
 
 //employee routes
-app.get('/api/employees',getAllEmployees)
-app.get('/api/employees/:id', getEmployeeById)
-app.post('/api/employees',createEmployee)
+app.get('/api/employees',authenticate,getAllEmployees)
+app.get('/api/employees/:id',authenticate, getEmployeeById)
+app.post('/api/employees',authenticate,createEmployee)
 app.put('/api/employees/:id',authenticate,authorize("employee","supervisor","IT"), updateEmployee)
 app.delete('/api/employees/:id',authenticate,authorize("employee","supervisor","IT"), deleteEmployee)
 
@@ -70,37 +70,37 @@ app.delete('/api/employees/:id',authenticate,authorize("employee","supervisor","
 
 
 //access request routes
-app.post('/api/accessRequests', createAccessRequest)
-app.get('/api/accessRequests',getAllAccessRequests)
-app.get('/api/accessRequests/:id', getAccessRequestById)
+app.post('/api/accessRequests',authenticate,authorize("employee","supervisor"), createAccessRequest)
+app.get('/api/accessRequests',authorize("employee","supervisor"),getAllAccessRequests)
+app.get('/api/accessRequests/:id', authorize("employee","supervisor"),getAccessRequestById)
 app.put('/api/accessRequests/:id',authenticate,authorize("employee","supervisor","IT"), updateAccessRequest)
 app.delete('/api/accessRequests/:id',authenticate,authorize("employee","supervisor","IT"), deleteAccessRequest)
 app.post('/api/accessRequests/:id/', authenticate,authorize("supervisor"),supervisorApproval)
-app.post('/api/accessRequests/:id/itApproval', itApproval)
-app.get('/api/statistics', getStatiticsByAdmin);
+app.post('/api/accessRequests/:id/itApproval',authorize("admin"), itApproval)
+app.get('/api/statistics', authenticate,getStatiticsByAdmin);
 app.get('/api/accessRequestsLimit', getAccessRequestLimit);
-app.get('/api/statistics/:employeeId', getStatitics);
-app.get('/api/popularSystemsPlatforms', getPopularSystemsPlatforms);
-app.get('/api/getStatisticsByAdmin', getStatiticsByAdmin);
-app.get('/api/accessRequests/employee/:userId', getAccessRequestsByEmployeeId);
-app.get('/api/supervisorDashboard/:id', getSupervisorTeamWithRequests)
-app.put('/api/supervisorApproaval/:id', supervisorApproval)
+app.get('/api/statistics/:employeeId',authenticate, getStatitics);
+app.get('/api/popularSystemsPlatforms',authenticate, getPopularSystemsPlatforms);
+app.get('/api/getStatisticsByAdmin',authenticate, getStatiticsByAdmin);
+app.get('/api/accessRequests/employee/:userId',authenticate, getAccessRequestsByEmployeeId);
+app.get('/api/supervisorDashboard/:id',authenticate, getSupervisorTeamWithRequests)
+app.put('/api/supervisorApproaval/:id',authenticate,authorize("supervisor"), supervisorApproval)
 
 
 
 //notification routes
 app.post('/api/notifications',authenticate, createNotification)
-app.get('/api/notifications',getAllNotifications)
+app.get('/api/notifications',authenticate,getAllNotifications)
 app.get('/api/notifications/:id',authenticate, getNotificationById)
-app.patch('/api/notifications/:id/markAsRead', markAsRead)
-app.delete('/api/notifications/:id', deleteNotification)
-app.get('/api/notifications/recipient/:recipientId', getNotificationsByRecipient);
-app.get('/api/notificationByEmail/:email',getNotificationCountsByEmail)
+app.patch('/api/notifications/:id/markAsRead',authenticate, markAsRead)
+app.delete('/api/notifications/:id',authenticate, deleteNotification)
+app.get('/api/notifications/recipient/:recipientId',authenticate, getNotificationsByRecipient);
+app.get('/api/notificationByEmail/:email',authenticate,getNotificationCountsByEmail)
 
 
 
 //dont touch this if your uncertain
-app.delete('/api/deseed',deSeeding)
+app.delete('/api/deseed',authenticate,authorize("admin"),deSeeding)
 
 
 
